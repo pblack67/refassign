13e36b6eaa7e7c22d8692cf9fe8d8e35591942a6
Paul Black <pblack67@comcast.net>
2019-06-24 13:00:10 -0500

    Added email field to Referee model, added start of seeds file for easier testing

--- models/referee.js
+++ models/referee.js
@@ -23,7 +23,14 @@ module.exports = function (sequelize, DataTypes) {
         zip: {
             type: DataTypes.STRING,
             allowNull: false
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false
         }
     });
     return Referee;
 };
+
+// Sample distance query for Google Maps Disane Matrix API
+// https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=Chicago+IL&destinations=Schaumburg+IL&key=AIzaSyBVt5b23HEgQxrwI2eeecCLLj5Yt-SDMdw
