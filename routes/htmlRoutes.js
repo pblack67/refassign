5bd7e37951eb6099ae929f3ddae5cf531310db5a
abdul wahid hashoory <whashoory@gmail.com>
2019-06-26 14:02:01 -0500

    added handlebars and style

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -19,6 +19,18 @@ module.exports = function(app) {
       });
     });
   });
+  app.get('/game', function(req, res){
+    res.render('game');
+  });
+  app.get('/referee', function(req, res){
+    res.render('referee');
+  });
+  app.get('/login', function(req, res){
+    res.render('login');
+  });
+  app.get('/contact', function(req, res){
+    res.render('contact');
+  });
 
   // Render 404 page for any unmatched routes
   app.get("*", function(req, res) {
