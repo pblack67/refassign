eccd0fde7d11ec82d9db9b1b2b579d5f0171adf5
Paul Black <pblack67@comcast.net>
2019-06-27 10:54:02 -0500

    Basic logic for synchronously querying all officials on a game. Still need to compare dates to see if official is available.

--- routes/api.js
+++ routes/api.js
@@ -1,4 +1,5 @@
 const db = require("../models");
+const moment = require("moment");
 
 function getAllReferees(callback) {
   db.Referees.findAll({}).then(dbReferees => {
@@ -12,10 +13,31 @@ function getAllGames(callback) {
   });
 }
 
-function getAllAvailableReferees(gameid, callback) {
-  db.Referees.findAll({}).then(dbReferees => {
-    callback(dbReferees);
+async function getAllAvailableReferees(gameid, callback) {
+  let availableReferees = [];
+  let dbReferees = await db.Referees.findAll({});
+  let gameToAssign = await db.Games.findOne({
+    where: { id: gameid }
   });
+//   console.log(gameToAssign.gameTime);
+//   let justDate = new String(gameToAssign.gameTime).split("T")[0];
+//   console.log(justDate);
+//   let gameMoment = moment(justDate);
+//   console.log(gameMoment);
+  for (let i = 0; i < dbReferees.length; i++) {
+    let referee = dbReferees[i];
+    let dbGames = await referee.getGames();
+    let available = true;
+    for (let j = 0; j < dbGames.length; j++) {
+      //   console.log(dbGames[j].schoolName);
+      // Compare referee games with potential assignment
+      // If the referee is "busy" then don't add him to the available list
+    }
+    if (available) {
+      availableReferees.push(referee);
+    }
+  }
+  callback(availableReferees);
 }
 
 module.exports = {
