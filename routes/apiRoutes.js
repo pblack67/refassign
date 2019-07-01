8209e2d7bfe023a54ea9d9cdbc399056fa554972
Paul Black <pblack67@comcast.net>
2019-07-01 12:34:54 -0500

    Put assignment screen into bootstrap columns, trying secure port for email to see if it works on Heroku

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -7,8 +7,10 @@ async function sendAssignmentMail(referee, game) {
   if (process.env.EMAIL_USER) {
     let transporter = nodemailer.createTransport({
       host: "smtp.gmail.com",
-      port: 587,
-      secure: false, // true for 465, false for other ports
+      // port: 587,
+      // secure: false, // true for 465, false for other ports
+      port: 465,
+      secure: true, // true for 465, false for other ports
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASSWORD
